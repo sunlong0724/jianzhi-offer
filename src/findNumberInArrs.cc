dd7e882e8566a6d475ccb6b9b2230f9af3157a63
sunruilong <sunlong0724@sina.com>
2015-09-04 00:04:59 +0800

    add another function

--- src/findNumberInArrs.cc
+++ src/findNumberInArrs.cc
@@ -41,6 +41,25 @@ bool findNumberInArrs(int** arrs, int rows, int cols, int num){
 	return false;
 }
 
+bool findNumberInArrs2(int ** arrs, int rows, int cols, int num) {
+
+	int row,col;
+	row = rows - 1;
+	col = 0;
+	while(row >= 0 && col < cols) {
+		int a = *((int*)arrs + row*cols + col);//A pionter of a tow-dimensional is not used for addressing by arrs[][] !!!
+		if ( num == a /*arrs[row][col] */) {
+			cout << "arr["<<row<<"]["<<col<<"]" << " is " << num<< endl;
+			return true;
+		}
+		else if (num > a/*arrs[row][col]*/)
+			col++;
+		else 
+			row--;
+	}
+	return false;
+}
+
 int main(int argc, char* argv[]){
 
 	int arrs[][4] = {
@@ -53,6 +72,8 @@ int main(int argc, char* argv[]){
 	string str;
 	while(getline(cin,str)){
 		cout << "find " << str << " in findNumberInArrs result is " << findNumberInArrs((int**)arrs,4,4,std::stoi(str)) << endl;
+
+		cout << "find " << str << " in findNumberInArrs2 result is " << findNumberInArrs2((int**)arrs,4,4,std::stoi(str)) << endl;
 
 	}
 
